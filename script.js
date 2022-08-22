fb4ab05e043b44d2ddccaa6bd4e0a1cc4c19039f
adicerbo <dicerboantonio32@gmail.com>
2022-08-22 15:23:00 -0400

    named p tags

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const url = "https://valorant-api.com/v1/agents"
 $.ajax(url).then(function(data) {
     console.log(data)
 })
+
+const $name = $(data.displayName)
+const $description = $('#description')
+const $portrait = $('#fullPortrait')
+const $role = $('#displayName')
+console.log($name)
+
+
+// const $name = $('#displayName')
+// const $name = $('#displayName')
+// const $name = $('#displayName')
+// const $name = $('#displayName')
+// const $name = $('#displayName')
+// const $name = $('#displayName')
+// const $name = $('#displayName')
