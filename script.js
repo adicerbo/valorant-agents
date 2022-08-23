9b5bf2390f2e0b986cb33dfb4000d31d40ac7a58
adicerbo <dicerboantonio32@gmail.com>
2022-08-23 13:11:36 -0400

    called agent names upon button press in one list

--- script.js
+++ script.js
@@ -1,28 +1,56 @@
 const URL = "https://valorant-api.com/v1/agents"
 
-$.ajax(URL).then(function(data) {
-    console.log(data)
-    let thisData = data.data
-    console.log(thisData)
+const $form = $('form')
+const $input = $('input [type = "text"]')
 
 
-    thisData.forEach(function(allData) {
-        // accessing all data
-        // console.log(allData.abilities)
-        let allAbilities = allData.abilities
-        
-        // accessing all abilities for each agent
-        allAbilities.forEach(function(abil) {
-            let abili = abil
-            // console.log(abil)
+// event listener
+$form.on('submit', handleGetData);
+
+function handleGetData(event) {
+    event.preventDefault()
+    userInput = $input.val()
+
+    $.ajax(URL).then(function (data) {
+        // console.log(data)
+        let thisData = data.data
+        // console.log(thisData)
+
+
+        thisData.forEach(function (allData) {
+            // accessing all data
+            // console.log(allData.abilities)
+            let allAbilities = allData.abilities
+
+            // accessing all abilities for each agent
+            allAbilities.forEach(function (abil) {
+                let abili = abil
+                // console.log(abil)
+            })
+
+            // accessing all names for agents
+            let allNames = allData.displayName
+            console.log(allNames)
+
+            // filtering out names
+            // let filteredName = allNames.filter()
+            // console.log(filteredName)
+
+            // accessing all descriptions for each agent
+            let allDesc = allData.description
+            // console.log(allDesc)
+
+            // accessing all primary images of agents
+            let allPortraits = allData.fullPortrait
+            // console.log(allPortraits)
+
+
         })
 
-        // accessing each name for agents
-        let allNames = allData.displayName
-        console.log(allNames)
-        
+
     })
-})
+
+}
 
 // const showAgent = (data) => {
 //     data.
