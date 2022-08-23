ffb84e24502af5ee532fa6e8295f0b9c28318deb
adicerbo <dicerboantonio32@gmail.com>
2022-08-22 20:48:56 -0400

    accessed api and called names/abilities

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
-const url = "https://valorant-api.com/v1/agents"
+const URL = "https://valorant-api.com/v1/agents"
 
-$.ajax(url).then(function(data) {
+$.ajax(URL).then(function(data) {
     console.log(data)
+    let thisData = data.data
+    console.log(thisData)
+
+
+    thisData.forEach(function(allData) {
+        // accessing all data
+        // console.log(allData.abilities)
+        let allAbilities = allData.abilities
+        
+        // accessing all abilities for each agent
+        allAbilities.forEach(function(abil) {
+            let abili = abil
+            // console.log(abil)
+        })
+
+        // accessing each name for agents
+        let allNames = allData.displayName
+        console.log(allNames)
+        
+    })
 })
 
-const $name = $(data.displayName)
-const $description = $('#description')
-const $portrait = $('#fullPortrait')
-const $role = $('#displayName')
-console.log($name)
+// const showAgent = (data) => {
+//     data.
+// }
 
 
+
+// const $name = $('#displayName')
+
+// $.ajax({
+//     url: "https://valorant-api.com/v1/agents"
+// })
+// .then(
+//     (data) => {
+//         $name.text(data.displayName)
+//     }
+// )
+
+// console.log($name)
+
+// const $description = $('#description')
+// const $portrait = $('#fullPortrait')
+// const $role = $('#displayName')
 // const $name = $('#displayName')
 // const $name = $('#displayName')
 // const $name = $('#displayName')
@@ -18,3 +53,4 @@ console.log($name)
 // const $name = $('#displayName')
 // const $name = $('#displayName')
 // const $name = $('#displayName')
+// const $input = $('input[type = "text"]')
